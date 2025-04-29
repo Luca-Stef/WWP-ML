2d6e2639eb5754a8cda13605b206b26324737881
Luca Stefanescu <lucastu2013@gmail.com>
2025-04-29 23:42:28 +0100

    Add files via upload

---
+++ models/LSTM/LSTM_316_09752336144447327.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void LSTM_316_09752336144447327(k2c_tensor* lstm_input_input, k2c_tensor* dense_1_output); 
+void LSTM_316_09752336144447327_initialize(); 
+void LSTM_316_09752336144447327_terminate(); 
